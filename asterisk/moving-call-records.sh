a6dd3c73622b6200fceb0173ab4ccc494a22e743
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-10-20 16:07:49 +0500

    Update moving-call-records.sh

--- asterisk/moving-call-records.sh
+++ asterisk/moving-call-records.sh
@@ -6,7 +6,7 @@ if ! [ -d /mnt/nfs/monitor/$datedir ]; then
 mkdir -p /mnt/nfs/monitor/$datedir
 fi
 cd /var/spool/asterisk/monitor/$datedir
-for i in $( find *.WAV ); do
+for i in $( find *.WAV -cmin +60 ); do
 mv /var/spool/asterisk/monitor/$datedir/$i /mnt/nfs/monitor/$datedir/$i
 ln -s /mnt/nfs/monitor/$datedir/$i /var/spool/asterisk/monitor/$datedir/$i
 chown asterisk. /var/spool/asterisk/monitor/$datedir -R
