c57328bc171b3d725ac7bbb9ea4ebe66026605d6
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-10-29 14:25:14 +0500

    Update check-nfs-share.sh

--- nfs/check-nfs-share.sh
+++ nfs/check-nfs-share.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 
 # Monitor for NFS file system mounted
 # Andrey Kuznetsov, 2022.10.29
@@ -7,6 +7,10 @@
 # WARNING! Our Linux server monitoring script should be installed in the network or on the specific host where the NFS mount is running!
 
 # Get user variables
+
+SRV=10.0.22.21
+TRAP=$(hostname -I)
+
 path=$1
 ipaddr=$(mount -l -t nfs,nfs4,nfs2,nfs3 | grep -w "$path" | awk -F'[(|,|=|)]' '{ for(i=1;i<=NF;i++) if ($i == "addr") print $(i+1) }')
 if [ "$ipaddr" ]
@@ -14,7 +18,7 @@ then
   echo "$ipaddr" |
   while read line
   do
-    df -k ${MP} &>/dev/null &
+    df -k ${MP} &> /dev/null &
     DFPID=$!
       for (( i=1 ; i<3 ; i++ ))
         do
@@ -29,10 +33,10 @@ then
       then
         $(kill -s SIGTERM $DFPID &> /dev/null)
         echo "0"
-        zabbix_sender -z 10.0.22.21 -s "10.1.1.3" -k nfs -o 0 > /dev/null 2>&1
+        zabbix_sender -z $SRV -s $TRAP -k check.nfs -o 0 > /dev/null 2>&1
       else
         echo "1"
-        zabbix_sender -z 10.0.22.21 -s "10.1.1.3" -k nfs -o 1 > /dev/null 2>&1
+        zabbix_sender -z $SRV -s $TRAP -k check.nfs -o 1 > /dev/null 2>&1
       fi
   done
 fi
