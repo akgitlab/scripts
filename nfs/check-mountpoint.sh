fd8dcea94c8293b7c4fbce5b494252537ff2ea8f
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-10-30 09:49:53 +0500

    Update check-mountpoint.sh

--- nfs/check-mountpoint.sh
+++ nfs/check-mountpoint.sh
@@ -6,13 +6,21 @@
 
 # WARNING! Our Linux server monitoring script should be installed in the network or on the specific host where the NFS mount is running!
 
+
 # Get user variables
 
 SRV=$(grep -v '^#' /etc/zabbix/zabbix_agentd.conf  | grep -v '^$' | grep Server= | awk -F '=' '{print $2}')
-TRAP=$(hostname -I | awk '{print $1}')
+TRAP=$(hostname -I)
+
+
+# Get Remote NFS server IP
 
 path=$1
 ipaddr=$(mount -l -t nfs,nfs4,nfs2,nfs3 | grep -w "$path" | awk -F'[(|,|=|)]' '{ for(i=1;i<=NF;i++) if ($i == "addr") print $(i+1) }')
+
+
+# Check if mountpoint exists
+
 if [ "$ipaddr" ]
 then
   echo "$ipaddr" |
@@ -32,8 +40,12 @@ then
       if ps -p $DFPID > /dev/null
       then
         $(kill -s SIGTERM $DFPID &> /dev/null)
+        echo -e "\033[0m\033[0m\033[31mRemote NFS volume is not mounted"
+        tput sgr0
         zabbix_sender -z $SRV -s $TRAP -k vfs.fs.mp -o 0 > /dev/null 2>&1
       else
+        echo -e "\033[32mRemote NFS volume is mounted"
+        tput sgr0
         zabbix_sender -z $SRV -s $TRAP -k vfs.fs.mp -o 1 > /dev/null 2>&1
       fi
   done
