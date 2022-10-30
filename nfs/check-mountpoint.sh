22ef1fe6e723d08db493f5cd8622bcfcba8b3ccd
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-10-30 05:38:21 +0500

    Update check-mountpoint.sh

--- nfs/check-mountpoint.sh
+++ nfs/check-mountpoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Monitor for NFS file system mounted
-# Andrey Kuznetsov, 2022.10.29
+# Andrey Kuznetsov, 2022.10.30
 # Telegram: https://t.me/akmsg
 
 # WARNING! Our Linux server monitoring script should be installed in the network or on the specific host where the NFS mount is running!
 
 # Get user variables
 
-SRV=10.0.22.21
-TRAP=$(hostname -I)
+SRV=$(grep -v '^#' /etc/zabbix/zabbix_agentd.conf  | grep -v '^$' | grep Server= | awk -F '=' '{print $2}')
+TRAP=$(hostname -I | awk '{print $1}')
 
 path=$1
 ipaddr=$(mount -l -t nfs,nfs4,nfs2,nfs3 | grep -w "$path" | awk -F'[(|,|=|)]' '{ for(i=1;i<=NF;i++) if ($i == "addr") print $(i+1) }')
@@ -32,11 +32,9 @@ then
       if ps -p $DFPID > /dev/null
       then
         $(kill -s SIGTERM $DFPID &> /dev/null)
-        echo "0"
-        zabbix_sender -z $SRV -s $TRAP -k vfs.fs.mounted -o 0 > /dev/null 2>&1
+        zabbix_sender -z $SRV -s $TRAP -k vfs.fs.mp -o 0 > /dev/null 2>&1
       else
-        echo "1"
-        zabbix_sender -z $SRV -s $TRAP -k vfs.fs.mounted -o 1 > /dev/null 2>&1
+        zabbix_sender -z $SRV -s $TRAP -k vfs.fs.mp -o 1 > /dev/null 2>&1
       fi
   done
 fi
