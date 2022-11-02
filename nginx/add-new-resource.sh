fc3dba6e2e2575543cc9ac5c01c4af8ba5826503
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-11-02 10:20:44 +0500

    Update add-new-resource.sh

--- nginx/add-new-resource.sh
+++ nginx/add-new-resource.sh
@@ -1,29 +1,78 @@
 #!/bin/bash
 
 # Adding a new resource to reverce-proxy server
+
 # Andrey Kuznetsov, 2022.10.26
 # Telegram: https://t.me/akmsg
 
 # Get variables
-FDQN=$1
-SRV=$2
-CRT=$3
-KEY=$4
 DATE=$(date  +%Y)
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]
+then
+  echo -e "\033[0m\033[0m\033[31mError: this script must be run as root!"
+  tput sgr0
+  exit 1
+fi
+
+# Get received user variables
+read -r -p "Enter fully qualified domain name: " FDQN
+
+# Check for domain name availability
+echo "Domain existence check by using WhoIS service..."
+whois $FDQN | grep "No entries found" > /dev/null 2>&1
+if [ $? -eq 0 ]
+then
+  echo -e "\033[0m\033[0m\033[31mError: could not find records for this domain!"
+  tput sgr0
+  exit 1
+else
+  echo -e "\033[32mSuccess: records found for this domain. Continue..."
+  tput sgr0
+fi
+
 # Configuration file existence check
-if [ -e /etc/nginx/sites-available/$FDQN ]
+if [ -f /etc/nginx/sites-available/$FDQN ]
 then
   echo -e "\033[0m\033[0m\033[31mError: the configuration for the resource already exists!"
   tput sgr0
+  exit 1
 else
 
+# Get received user variables
+read -r -p "Enter the IP address of the server on the internal network: " SRV
+
 # Start destination server ping check
+echo "Destination server ping check..."
 ping -c 3 $SRV  2>&1 > /dev/null
 if [ $? -ne 0 ]
 then
   echo -e "\033[0m\033[0m\033[31mError: destination server is not available, check it before next script run!"
   tput sgr0
+  exit 1
+else
+
+# Get received user variables
+read -r -p "Enter the name of the fullchain certificate file (for example: star.5-55.ru.bundle.pem): " CRT
+
+# Certificate existence check
+if [ ! -f /etc/nginx/certs/$DATE/$CRT ]
+then
+  echo -e "\033[0m\033[0m\033[31mError: certificate file not found in directory /etc/nginx/certs/$DATE/ !"
+  tput sgr0
+  exit 1
+else
+
+# Get received user variables
+read -r -p "Enter the name of the private key file (for example: star.5-55.ru.key): " KEY
+
+# Key file existence check
+if [ ! -f /etc/nginx/certs/$DATE/$KEY ]
+then
+  echo -e "\033[0m\033[0m\033[31mError: key file not found in directory /etc/nginx/certs/$DATE/ !"
+  tput sgr0
+  exit 1
 else
 
 # Start creating configuration file for new site
@@ -63,15 +112,28 @@ server {
 EOF
 ) >  /etc/nginx/sites-available/$FDQN
 fi
+fi
+fi
+
+# User confirmation request
+read -r -p "Do you really want to create a config file and activate the resource now? [y/n] " response
+if ! [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+then
+  rm /etc/nginx/sites-available/"$FDQN"
+  echo -e "\033[0m\033[0m\033[31mError: Resource publish setup aborted by user!"
+  tput sgr0
+  exit 1
+else
 
 # Final stage of action
 echo "Making symbolic link..."
 ln -s /etc/nginx/sites-available/"$FDQN" /etc/nginx/sites-enabled/"$FDQN"
 echo "Reload NGING proxy service..."
 /etc/init.d/nginx reload > /dev/null 2>&1
+
 if [ $? -eq 0 ]
 then
-  echo -e "\033[32mService Nginx restart completed and new site has been setup! Enjoy..."
+  echo -e "\033[32mSuccess: service Nginx restart completed and new site has been setup!"
   tput sgr0
 else
   rm /etc/nginx/sites-available/"$FDQN"
@@ -79,5 +141,6 @@ else
   /etc/init.d/nginx reload > /dev/null 2>&1
   echo -e "\033[0m\033[0m\033[31mError: check the existence and names of the certificates and try again later..."
   tput sgr0
+fi
 fi
 fi
