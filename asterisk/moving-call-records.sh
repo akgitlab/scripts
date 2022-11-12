72f31b1b17306153e588ba5884cba3f223db71b2
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-11-12 14:02:08 +0500

    Update moving-call-records.sh

--- asterisk/moving-call-records.sh
+++ asterisk/moving-call-records.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 export
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+cdrdb="asteriskcdrdb"
+cdrtable="cdr"
+celtable="cel"
+dbuser="freepbx"
+dbpass="pL0i#b3Y6tz"
 datedir=$(date +"%Y/%m/%d")
+
 if ! [ -d /mnt/nfs/monitor/$datedir ]; then
-mkdir -p /mnt/nfs/monitor/$datedir
+  mkdir -p /mnt/nfs/monitor/$datedir
 fi
+
 cd /var/spool/asterisk/outgoing/$datedir
-for i in $( find *.mp3 ); do
-mv /var/spool/asterisk/outgoing/$datedir/$i /mnt/nfs/monitor/$datedir/$i
-chown -R asterisk. /var/spool/asterisk/monitor/$datedir
+
+for i in `find ./ -type f -name "*.mp3"`
+  do
+    if [ -e "$i" ]
+      then
+        file=`basename "$i" .mp3`;
+        mysql --user="$dbuser" --password="$dbpass" --database="$cdrdb" --execute='UPDATE '$cdrtable' SET \
+        recordingfile="'$file'.mp3" WHERE recordingfile="'$file'.wav";';
+        mv /var/spool/asterisk/outgoing/$datedir/$i /mnt/nfs/monitor/$datedir/$i
+        chown -R asterisk. /var/spool/asterisk/monitor/$datedir
+    fi
 done
