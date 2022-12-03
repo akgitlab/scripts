59139c87b441ba5d577b170416fbcb1aef69283e
Andrey K <99391509+akgitlab@users.noreply.github.com>
2022-12-03 14:54:03 +0500

    Update publish.sh

--- nginx/publish.sh
+++ nginx/publish.sh
@@ -7,36 +7,44 @@
 
 # Get variables
 DATE=$(date  +%Y)
-EXTIP=$(curl eth0.me > /dev/null 2>&1)
+EXTIP=$(curl eth0.me 2>&1)
+FDQN=$1
 
 # Make sure only root can run our script
 if [[ $EUID -ne 0 ]]
 then
-  echo -e "\033[0m\033[0m\033[31mError: this script must be run as root!"
+  echo -e "\033[0m\033[0m\033[31mError: This script must be run as root!"
   tput sgr0
   exit 1
 fi
 
-# Get received user variables
-read -r -p "Enter fully qualified domain name: " FDQN
+# Check received user variables
+if [[ $FDQN = "" ]]
+then
+  echo -e "\033[0m\033[0m\033[31mError: Fully qualified domain name required!"
+  tput sgr0
+  exit 1
+fi
 
 # Check for domain name availability
 echo "Domain DNS records existence check by using DIG service..."
-dig $FDQN +noall +answer | grep $EXTIP > /dev/null 2>&1
+dig $FDQN +noall +answer | grep "$EXTIP" > /dev/null 2>&1
 if [ $? -eq 1 ]
 then
-  echo -e "\033[0m\033[0m\033[31mError: could not find records for this domain!"
+  echo -e "\033[0m\033[0m\033[31mError: Current external IP address does not match the DNS entry or the entry is missing!"
   tput sgr0
   exit 1
 else
-  echo -e "\033[32mSuccess: records found for this domain. Continue..."
+  echo -e "\033[32mSuccess: DNS records found for this domain. Continue..."
   tput sgr0
 fi
 
 # Configuration file existence check
+echo "Configuration file existence check..."
+sleep 2
 if [ -f /etc/nginx/sites-available/$FDQN ]
 then
-  echo -e "\033[0m\033[0m\033[31mError: the configuration for the resource already exists!"
+  echo -e "\033[0m\033[0m\033[31mError: The configuration for the resource already exists!"
   tput sgr0
   exit 1
 else
@@ -49,7 +57,7 @@ echo "Destination server ping check..."
 ping -c 3 $SRV  2>&1 > /dev/null
 if [ $? -ne 0 ]
 then
-  echo -e "\033[0m\033[0m\033[31mError: destination server is not available, check it before next script run!"
+  echo -e "\033[0m\033[0m\033[31mError: Destination server is not available, check it before next script run!"
   tput sgr0
   exit 1
 else
@@ -60,7 +68,7 @@ read -r -p "Enter the name of the fullchain certificate file (for example: star.
 # Certificate existence check
 if [ ! -f /etc/nginx/certs/$DATE/$CRT ]
 then
-  echo -e "\033[0m\033[0m\033[31mError: certificate file not found in directory /etc/nginx/certs/$DATE/ !"
+  echo -e "\033[0m\033[0m\033[31mError: Certificate file not found in directory /etc/nginx/certs/$DATE/ !"
   tput sgr0
   exit 1
 else
@@ -71,7 +79,7 @@ read -r -p "Enter the name of the private key file (for example: star.5-55.ru.ke
 # Key file existence check
 if [ ! -f /etc/nginx/certs/$DATE/$KEY ]
 then
-  echo -e "\033[0m\033[0m\033[31mError: key file not found in directory /etc/nginx/certs/$DATE/ !"
+  echo -e "\033[0m\033[0m\033[31mError: Key file not found in directory /etc/nginx/certs/$DATE/ !"
   tput sgr0
   exit 1
 else
@@ -83,7 +91,7 @@ cat <<EOF
 server {
     listen 80;
     server_name $FDQN;
-    return 301 https://"$host$request_uri";
+    return 301 https://\$host\$request_uri;
 }
 
 server {
@@ -130,17 +138,18 @@ else
 echo "Making symbolic link..."
 ln -s /etc/nginx/sites-available/"$FDQN" /etc/nginx/sites-enabled/"$FDQN"
 echo "Reload NGING proxy service..."
+nginx -t > /var/log/nginx/reload.log 2>&1
 /etc/init.d/nginx reload > /dev/null 2>&1
 
 if [ $? -eq 0 ]
 then
-  echo -e "\033[32mSuccess: service Nginx restart completed and new site has been setup!"
+  echo -e "\033[32mSuccess: Service Nginx restart completed and new site has been setup!"
   tput sgr0
 else
   rm /etc/nginx/sites-available/"$FDQN"
   rm /etc/nginx/sites-enabled/"$FDQN"
   /etc/init.d/nginx reload > /dev/null 2>&1
-  echo -e "\033[0m\033[0m\033[31mError: check the existence and names of the certificates and try again later..."
+  echo -e "\033[0m\033[0m\033[31mError: Check the existence and names of the certificates and try again later..."
   tput sgr0
 fi
 fi
