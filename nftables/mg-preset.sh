63525375dceb29afc874b1bdb022f37e7b5a8b4d
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-01-12 15:27:20 +0500

    Update mg-preset.sh

--- nftables/mg-preset.sh
+++ nftables/mg-preset.sh
@@ -30,18 +30,17 @@ then
         ${nft} add rule inet filter input ip saddr 10.3.44.0/24 tcp dport 22 counter accept
         ${nft} add rule inet filter input ip saddr 10.250.250.0/24 tcp dport 22 counter accept
         ${nft} add rule inet filter input ip saddr 10.3.44.0/24 tcp dport { 80, 443 } counter accept
-        ${nft} add rule inet filter input ip saddr 10.0.22.21/32 tcp dport { 22, 80, 443, 5038, 8088, 10050 } counter accept
-        ${nft} add rule inet filter input tcp dport { 22, 80, 443, 5038, 8088, 10050 } counter drop
-        #-----------------sip provider input-------------------#
-        ${nft} add rule inet filter input ip saddr 92.53.82.188 tcp dport 25 counter drop
-        ${nft} add rule inet filter input ip tcp dport 25 counter accept
+        ${nft} add rule inet filter input ip saddr 10.0.22.21/32 tcp dport { 22, 80, 443, 10050 } counter accept
+        ${nft} add rule inet filter input tcp dport { 22, 80, 443, 10050 } counter drop
+        #--------------------client input----------------------#
+        ${nft} add rule inet filter input ip saddr 92.53.82.188/32 tcp dport 25 counter drop
+        ${nft} add rule inet filter input ip saddr 0.0.0.0/0 tcp dport 25 counter accept
         #----------------------forward-------------------------#
         ${nft} add rule inet filter forward ct state new tcp dport 25 counter accept
         ${nft} add rule inet filter forward counter reject with icmp type host-prohibited
         #-----------------------output-------------------------#
         ${nft} add rule inet filter output counter accept
         #------------------------drop--------------------------#
-        ${nft} add rule inet filter input ct state new tcp dport 25 counter drop
         ${nft} add rule inet filter input counter reject with icmp type host-prohibited
 
         ${nft} -s list ruleset | tee /etc/nftables.conf > /dev/null 2>&1
