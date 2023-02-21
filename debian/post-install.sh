c96d3a56a4b7fef149d4e6dca9c254108f93a0f8
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-02-21 21:46:12 +0500

    Update post-install.sh

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -16,7 +16,52 @@ WHITE_R='\033[39m'
 RED='\e[31m'
 GREEN='\e[32m'
 
+# Set logfile path
+LOG="/var/log/post-install.log"
+
 # Get variables
 
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]
+then
+  echo -e "\033[0m\033[0m\033[31mError: This script must be run only as root or privileged user!"
+  tput sgr0
+  exit 1
+fi
+
+# Start a post-install script
+echo -e "\n$(date '+%d/%m/%Y %H:%M:%S') [info] User $USER start a post-install script" >> $LOG
+
+timedatectl set-timezone Europe/Moscow
+
+# Repository
+
+
+
+
+
+apt update && apt -y upgrade
+
+apt -y install sudo mc htop screen screenfetch ncdu gnupg curl wget
+
+
+
+# Install and minimal configuration zabbix-agent
+if [ -x /usr/bin/apt-get ]; then
+  cd /tmp
+  wget https://repo.zabbix.com/zabbix/6.0/debian/pool/main/z/zabbix-release/zabbix-release_6.0-4%2Bdebian11_all.deb
+  dpkg -i zabbix-release_6.0-4+debian11_all.deb
+  apt update
+  apt-get -y install zabbix-agent
+  sed -i 's/Server=127.0.0.1/Server=10.0.22.21/' /etc/zabbix/zabbix_agentd.conf
+  systemctl restart zabbix-agent
+  systemctl enable zabbix-agent
+fi
+
+
+
+sudo apt -y autoremove
+sudo apt -y clean
 
 https://github.com/ahmetcancicek/debian-post-install/blob/main/install-sudo.sh
