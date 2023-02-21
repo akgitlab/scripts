736cc97984675e4e773d56576cf406486494b14a
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-02-21 21:07:18 +0500

    Update post-install.sh

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+
+
+RESET='\033[0m'
+YELLOW='\033[1;33m'
+GRAY='\033[0;37m'
+WHITE='\033[1;37m'
+GRAY_R='\033[39m'
+WHITE_R='\033[39m'
+RED='\033[1;31m' # Light Red.
+GREEN='\033[1;32m' # Light Green.
