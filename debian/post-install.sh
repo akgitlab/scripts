5770a8cba3d069118274d9e1dc9d73e5928b5680
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-02-21 21:21:35 +0500

    Update post-install.sh

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+# After install script for debian server
+# Andrey Kuznetsov, 2023.02.21
+# Telegram: https://t.me/akmsg
 
+# WARNING! Carefully check all the settings, because by applying this script you can block your access to the server!
 
+# Set messages colors
 RESET='\033[0m'
 YELLOW='\033[1;33m'
 GRAY='\033[0;37m'
 WHITE='\033[1;37m'
 GRAY_R='\033[39m'
 WHITE_R='\033[39m'
-RED='\033[1;31m' # Light Red.
-GREEN='\033[1;32m' # Light Green.
+RED='\e[31m'
+GREEN='\e[32m'
+
+# Get variables
+
+
+https://github.com/ahmetcancicek/debian-post-install/blob/main/install-sudo.sh
