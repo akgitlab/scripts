796349291d6bacb8b80986adfe619b606a62f174
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-02-25 09:43:09 +0500

    Update post-install.sh

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -175,6 +175,34 @@ sed -i 's/^#PermitRootLogin .*/PermitRootLogin no/' /etc/ssh/sshd_config
 sed -i 's/^#PrintLastLog .*/PrintLastLog no/' /etc/ssh/sshd_config
 
 
+# User audit
+(
+cat <<EOF
+
+# User shell activity audit (paste in /etc/bash.bashrc)
+PREF="audit"
+RUSER=$(who | awk '{print $1}')
+IP=$(who am i | awk '{ print $5 }' | sed 's/(//g' | sed 's/)//g')
+function h2log {
+  declare CMD
+  declare _PWD
+    CMD=$(history 1)
+    CMD=$(echo $CMD | awk '{print substr($0,length($1)+2)}')
+    _PWD=$(pwd)
+      if [ "$CMD" != "$pCMD" ]; then
+        logger -p local7.notice -t bash -- "user \"${RUSER}\" from source ip \"${IP}\" being in the directory \"${_PWD}\" executed on behalf \
+        of \"${USER}\" the shell command: \"${CMD}\""
+      fi
+    pCMD=$CMD
+}
+trap h2log DEBUG || EXIT
+EOF
+) >>  /etc/bash.bashrc
+
+mkdir /var/log/bash
+echo "local7.* /var/log/bash/audit.log" > /etc/rsyslog.d/audit.conf
+
+
 # Finish actions
 apt -y autoremove
 apt -y clean
