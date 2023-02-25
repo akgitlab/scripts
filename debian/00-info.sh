66a2fb9facbddefdfac618a6fc7c3d3abb56c670
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-02-25 10:16:46 +0500

    Update 00-info.sh

--- debian/00-info.sh
+++ debian/00-info.sh
@@ -12,7 +12,6 @@ cat /etc/update-motd.d/00-msg
 printf "\n%s\n" "$(date)"
 printf "Distro: %s | Core: %s\n" "$PRETTY_NAME" "$(uname -r)"
 echo "Powered by Debian is a distribution of Free Software and maintained and updated through the work of many users who volunteer..."
-echo ""
 
 # Show failed services
 systemctl list-units --state failed --type service | awk 'FNR>1' | grep failed
