529f27e4e8d9bd9911080f68743e4abf45673766
Andrey K <99391509+akgitlab@users.noreply.github.com>
2023-12-21 11:23:24 +0500

    Update post-install.sh

--- debian/post-install.sh
+++ debian/post-install.sh
@@ -138,20 +138,20 @@ if [ -x /usr/bin/apt-get ]; then
 fi
 
 # Install Graylog sidecar
-cd /tmp
-wget https://packages.graylog2.org/repo/packages/graylog-sidecar-repository_1-5_all.deb
-dpkg -i graylog-sidecar-repository_1-5_all.deb
-apt update && apt install graylog-sidecar
-curl https://raw.githubusercontent.com/akgitlab/files/main/graylog/sidecar/linux/debian/config/sidecar.yml > /etc/graylog/sidecar/sidecar.yml
-graylog-sidecar -service install
-systemctl enable graylog-sidecar && systemctl start graylog-sidecar
+#cd /tmp
+#wget https://packages.graylog2.org/repo/packages/graylog-sidecar-repository_1-5_all.deb
+#dpkg -i graylog-sidecar-repository_1-5_all.deb
+#apt update && apt install graylog-sidecar
+#curl https://raw.githubusercontent.com/akgitlab/files/main/graylog/sidecar/linux/debian/config/sidecar.yml > /etc/graylog/sidecar/sidecar.yml
+#graylog-sidecar -service install
+#systemctl enable graylog-sidecar && systemctl start graylog-sidecar
 
 # Install Filebeat
-cd /tmp
-wget https://github.com/akgitlab/files/releases/download/filebeat/filebeat-8.6.2-amd64.deb
-dpkg -i filebeat-8.6.2-amd64.deb
-apt install filebeat
-systemctl enable filebeat && systemctl start filebeat
+#cd /tmp
+#wget https://github.com/akgitlab/files/releases/download/filebeat/filebeat-8.6.2-amd64.deb
+#dpkg -i filebeat-8.6.2-amd64.deb
+#apt install filebeat
+#systemctl enable filebeat && systemctl start filebeat
 
 # User setup
 /sbin/usermod -aG sudo $RUSER
