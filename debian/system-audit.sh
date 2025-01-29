176666b835d4ea5312a2a4a59c8d15b4d8dceeef
Andrey K. <99391509+akgitlab@users.noreply.github.com>
2025-01-29 08:47:00 +0500

    Update system-audit.sh

--- debian/system-audit.sh
+++ debian/system-audit.sh
@@ -4,7 +4,53 @@
 # Andrey Kuznetsov, 2025.01.29
 # Telegram: https://t.me/akmsg
 
-# This is a security audit bash script to gather instantly information about your Linux system which can also help you in the process of hardening.
+# This is a security audit bash script to gather instantly information about your Linux system
+# which can also help you in the process of hardening.
+
+
+# Set messages colors
+RESET='\033[0m'
+YELLOW='\033[1;33m'
+GRAY='\033[0;37m'
+WHITE='\033[1;37m'
+GRAY_R='\033[39m'
+WHITE_R='\033[39m'
+RED='\e[31m'
+GREEN='\e[32m'
+
+# So let's begin...
+
+# Clearing the screen from previously entered commands
+clear -x
+
+# Print script logo
+script_logo() {
+  cat << "EOF"
+__  ___/_____  ____________  /______ _______ ___ ______ _____  ________  /___(_)__  /_
+_____ \ __  / / /__  ___/_  __/_  _ \__  __ `__ \_  __ `/_  / / /_  __  / __  / _  __/
+____/ / _  /_/ / _(__  ) / /_  /  __/_  / / / / // /_/ / / /_/ / / /_/ /  _  /  / /_  
+/____/  _\__, /  /____/  \__/  \___/ /_/ /_/ /_/ \__,_/  \__,_/  \__,_/   /_/   \__/  
+        /____/                                                                          
+
+EOF
+}
+
+start_script() {
+  script_logo
+  echo -e "Welcome to security audit script of your Linux system by AK"
+  sleep 5
+}
+start_script
+
+# User privilege check
+if [[ $EUID -ne 0 ]]
+then
+  echo -e "\033[0m\033[0m\033[31mError: This script must be run only as root or privileged user!"
+  echo -e ""
+  tput sgr0
+  exit 1
+fi
+
 
 touch /tmp/audit/Рекомендации.txt
 hostnamectl > /tmp/audit/Описание.txt
