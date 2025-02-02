6203ce23530f3d1d8df98561e8375bf23487adc6
Andrey K. <99391509+akgitlab@users.noreply.github.com>
2025-02-02 18:08:36 +0500

    Update system-audit.sh

--- debian/system-audit.sh
+++ debian/system-audit.sh
@@ -30,11 +30,11 @@ fi
 
 #!/bin/bash
 
-# Security audit bash script for Linux systems
+# System audit scrip
 # Andrey Kuznetsov, 2025.01.29
 # Telegram: https://t.me/akmsg
 
-# This is a security audit bash script to gather instantly information about your Linux system
+# This is a system audit bash script to gather instantly information about your Linux system
 # which can also help you in the process of hardening.
 
 
@@ -48,14 +48,14 @@ WHITE_R='\033[39m'
 RED='\e[31m'
 GREEN='\e[32m'
 
+# Hostname
+RNAME=$(hostname | tr [:lower:] [:upper:])
+
 # Real username
 RUSER=$(who | awk '{print $1}' | head -1)
 
-# Hostname
-RNAME=$(hostname)
-
 # Set report file path
-REPORT="/home/$RUSER/$RNAME-audit.txt"
+REPORT="/tmp/$RNAME-audit.txt"
 
 
 # So let's begin...
@@ -78,10 +78,15 @@ EOF
 
 start_script() {
   script_logo
-  echo -e "Welcome to security audit script of your Linux system by AK"
-  sleep 5
+  echo -e "Welcome to system audit script of your Linux system by AK"
+  echo
+  echo "Script will automatically gather the required info."
+  echo "The checklist can help you in the process of hardening your system."
+  echo "It has been tested for Debian Linux Distro."
 }
 start_script
+
+sleep 3
 
 # User privilege check
 if [[ $EUID -ne 0 ]]
@@ -93,25 +98,98 @@ then
 fi
 
 # Request to save results
+echo
 while true; do
-    read -p "Would you like to save the output? [Y/N] " output
+    read -p "Would you like save the output to file? [Y/N] " output
     case "${output^^}" in
         Y)
-            echo "File will be saved to $REPORT"
+            echo "Result will be saved to $REPORT"
             break
             ;;
         N)
-            echo "OK, not saving moving on."
+            echo "Result will not be saved, moving on."
             break
             ;;
         *)
-            echo "Invalid input. Please enter Y or N."
+            echo -e "\033[0m\033[0m\033[31mError: Incorrect value entered."
+            echo -e ""
+            tput sgr0
             ;;
     esac
 done
 
-# Date and time start audit
-echo -e "\n$(date '+%d/%m/%Y %H:%M:%S')" >> $REPORT
+echo
+echo "System audit for $RNAME let's continue, please wait for it to finish."
+START=$(date +%s)
+echo
+
+# Function to perform audit
+perform_audit() {
+
+# Date and time start
+echo -e "\e[0;33mScript started at $(date '+%d/%m/%Y %H:%M:%S')\e[0m"
+echo
+echo -e "\e[0;33m##### 1. Hardware information #####\e[0m"
+    hostnamectl | sed -e 's/^[^[:alpha:]]\+//' | grep -vE "(Icon|hostname|Kernel|Operating|Firmware|ID)" | sort -n
+echo
+echo -e "\e[0;33m##### 2. Kernel information #####\e[0m"
+    cat /proc/sys/kernel/{ostype,osrelease,version}
+    # or "uname -a"
+echo
+echo -e "\e[0;33m##### 3. Distribution information #####\e[0m"
+    lsb_release -a | sed -e "s/[[:space:]]\+/ /g"
+    echo "Installed at: $(ls -lct --time-style=+"%d/%m/%Y %H:%M:%S" / | tail -1 | awk '{print $6, $7}')"
+echo
+
+echo -e "\e[0;33m##### 3. Time zone information #####\e[0m"
+    timedatectl | grep zone | sed -e 's/^[^[:alpha:]]\+//'
+    # or "timedatectl status | grep "zone" | sed -e 's/^[ ]*Time zone: \(.*\) (.*)$/\1/g'"
+    # or "timedatectl | sed -n 's/^\s*Time zone: \(.*\) (.*/\1/p'"
+    # or "cat /etc/timezone"
+echo
+
+echo -e "\e[0;33m##### X. Secure shell settings #####\e[0m"
+if [ -f /etc/ssh/sshd_config ]; then
+    grep -E 'PermitRootLogin|PasswordAuthentication|PubkeyAuthentication|Listen|Port' /etc/ssh/sshd_config
+else
+    echo "Configuration file not found."
+fi
+echo
+
+echo -e "\e[0;33m##### X. Users information #####\e[0m"
+    echo "All users (excluding system users):"
+    getent passwd | awk -F: '{if($7=="/bin/bash")print $1}' | grep -wv root
+    echo "Privileged users:"
+    getent group sudo | cut -d: -f4 | sed '/^[[:space:]]*$/d'
+    # For CentOS "lid -g wheel --"
+    echo "Users with null passwords:"
+    cat /etc/shadow | awk -F: '($2 == "" ) {print $1}' | sed '/^[[:space:]]*$/d'
+    echo "Users may run the following commands:"
+    sudo -l | sed '/^[[:space:]]*$/d' | sed -e 's/^[^[:alpha:]]\+//'
+
+
+
+}
+
+# Function write to file
+if [[ "${output^^}" == "Y" ]]; then
+    perform_audit >> $REPORT
+    chown $RUSER:$RUSER $REPORT
+else
+    perform_audit
+fi
+
+echo
+echo "################################################################"
+echo
+END=$(date +%s)
+DIFF=$(( END - START ))
+echo "Script completed in $DIFF seconds."
+echo
+
+exit 0
+
+
 
 ### Old script ###
 touch /tmp/audit/Рекомендации.txt
